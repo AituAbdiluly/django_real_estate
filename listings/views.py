982be57f8cc50af7499fb2d49ed9343ef2d5893e
Aituar Abdiluly <aituar.abdiluly@teskalabs.com>
2020-04-29 13:22:28 +0200

    listings, listing and etc.

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
+from .models import Listing
+from realtors.models import Realtor
+
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings
+    }
+
+    return render(request, 'listings/listings.html', context)
+
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+
+    listing = get_object_or_404(Listing, pk=listing_id)
+    mvp_realtors = Realtor.objects.filter(is_mvp=True)
+
+    context = {
+        'listing': listing,
+        'mvp_realtors': mvp_realtors
+    }
+
+    return render(request, 'listings/listing.html', context)
+
 
 def search(request):
     return render(request, 'listings/search.html')
--- realtors/admin.py
+++ realtors/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Realtor
 
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'hire_date', 'email')
+    list_filter = ('hire_date',)
+
+admin.site.register(Realtor, RealtorAdmin)

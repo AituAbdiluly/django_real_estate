97236dceffcd3939bb7eb8832b01901dcaa32fec
Aituar Abdiluly <aituar.abdiluly@teskalabs.com>
2020-05-01 12:27:09 +0200

    Removed template sample dir, Login page, Register page, Dashboard page

---
+++ accounts/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+
+
+def login(request):
+    if request.method == 'POST':
+        # Login user
+        # Get form values
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Your username or password is not correct.')
+            return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are logged out now.')
+        return redirect('index')
+
+
+def registration(request):
+    if request.method == 'POST':
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check if passwords match
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken.')
+                return redirect('registration')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'That email is being used.')
+                return redirect('registration')
+            else:
+                # Create user
+                user = User.objects.create_user(
+                    username=username, email=email, password=password, first_name=first_name, last_name=last_name)
+
+                # Login after register
+                # auth.login(request, user)
+                # messages.success(request, 'You are now logged in.')
+                # return redirect('index')
+
+                user.save()
+                messages.success(request, 'You are now logged in.')
+                return redirect('login')
+
+        else:
+            messages.error(request, 'Passwords do not match.')
+            return redirect('registration')
+
+    else:
+        return render(request, 'accounts/registration.html')
+
+
+def dashboard(request):
+    if not request.user.is_authenticated:
+        return redirect('index')
+    else:
+        return render(request, 'accounts/dashboard.html')
--- btre/settings.py
+++ btre/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
@@ -134,3 +135,8 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
